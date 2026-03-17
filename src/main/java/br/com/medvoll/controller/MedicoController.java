32fdb00e8960cd623aff27c4b3fd2af01d657751
Gabriel <50024453+TheGabrielVieira@users.noreply.github.com>
2026-03-17 01:47:18 -0300

    feat: adicionando dto para dados de listagem de medicos e alterando o metodo get da controller de medico para a paginacao

--- src/main/java/br/com/medvoll/controller/MedicoController.java
+++ src/main/java/br/com/medvoll/controller/MedicoController.java
@@ -1,15 +1,17 @@
 package br.com.medvoll.controller;
 
 import br.com.medvoll.medico.DadosCadastroMedico;
+import br.com.medvoll.medico.DadosListagemMedico;
 import br.com.medvoll.medico.Medico;
 import br.com.medvoll.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,6 +24,12 @@ public class MedicoController {
     @Transactional
     public void cadastroMedico(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listarMedicos(Pageable paginacao){
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
 
     }
 }
---
+++ src/main/java/br/com/medvoll/medico/DadosListagemMedico.java
@@ -0,0 +1,10 @@
+package br.com.medvoll.medico;
+
+import br.com.medvoll.endereco.Endereco;
+
+public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
